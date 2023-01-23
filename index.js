abdd6e7bf692cf34e305df4d0ce3bd1b675cb2f8
omar <omar.nabhan1958@gmail.com>
2023-01-22 23:54:47 -0500

    commit tree 3378a05b277eb938680b9733527be9820364e231

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+let countdown = 0;
+while (countdown < 10) {
+  console.log(countdown++);
+}
+for (let countdown = 0; countdown < 10; countdown++) {
+    console.log(countdown);
+  }
+  
